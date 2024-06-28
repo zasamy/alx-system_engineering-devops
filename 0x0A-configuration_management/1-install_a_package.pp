2a58a531b19165862fa92072698af6fb26c500af
zasamy <zainebzahiri@gmail.com>
2024-06-28 02:38:38 -0700

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-# script for installing lint
-
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
